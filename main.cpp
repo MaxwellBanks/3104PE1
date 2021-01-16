30216fcb8d25101cda4590fe957e0c4c2ecb5e95
Maxwell.Banks <maxwell.banks@colorado.edu>
2021-01-16 16:03:18 -0700

    Code now functions according to specifications

--- main.cpp
+++ main.cpp
@@ -1,21 +1,42 @@
 #include "Point.h"
 #include <iostream>
 
-int main(){
-    Point point1(5,4);
-    Point point2(3,8);
-    Point point3(1,9);
-
+void PrintPointCoordinates(Point point1, Point point2, Point point3){
+    std::cout << "Point Coordinates" << std::endl;
     std::cout << "Point 1: (" << point1.GetXCoordinate() << "," << point1.GetYCoordinate() << ")" << std::endl;
     std::cout << "Point 2: (" << point2.GetXCoordinate() << "," << point2.GetYCoordinate() << ")" << std::endl;
     std::cout << "Point 3: (" << point3.GetXCoordinate() << "," << point3.GetYCoordinate() << ")" << std::endl;
     std::cout << std::endl;
+}
 
+void PrintPointDistances(Point point1, Point point2, Point point3){
     std::cout << "Distance between Points:" << std::endl;
     std::cout << "1 and 2: " << point1.Distance(point2) << std::endl;
     std::cout << "2 and 3: " << point2.Distance(point3) << std::endl;
     std::cout << "3 and 1: " << point3.Distance(point1) << std::endl;
     std::cout << std::endl;
+}
+
+int main(){
+    Point first_point(5,4);
+    Point second_point(3,8);
+    Point third_point(1,9);
+
+    PrintPointCoordinates(first_point, second_point, third_point);
+    PrintPointDistances(first_point, second_point, third_point);
+
+    std::cout << "Translating point 1 by 7" << std::endl << std::endl;
+    first_point.Translate(7);
+
+    PrintPointCoordinates(first_point, second_point, third_point);
+    PrintPointDistances(first_point, second_point, third_point);
+
+    std::cout << "Translating point 1 by 7" << std::endl << std::endl;
+    first_point.Translate(-3.6);
+
+    PrintPointCoordinates(first_point, second_point, third_point);
+    PrintPointDistances(first_point, second_point, third_point);
+
 
     return 0;
 }
